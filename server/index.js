52a5708f06020ee721d04369497a52876d939d90
Veronique Doss <vdoss@student.21-school.ru>
2022-12-20 06:33:01 +0700

    backend auth middleware finished

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const sequelize = require('./db')
 const router = require('./routes/index');
 const errorHandler = require('./middleware/errorHandlingMiddleware');
+const { Admin } = require('./models/models');
 
 const PORT = process.env.port || 5000
 const app = express();
@@ -18,7 +19,17 @@ app.use(errorHandler)       // Обработчик ошибок
 const start = async () => {
     try {
         await sequelize.authenticate()  // подключение к PostgreSQL через sequelize 
-        await sequelize.sync()          
+        await sequelize.sync()   
+        
+        // создадим самого первого администратора
+        try {
+            await Admin.create({login: "root", password: "root"});
+            console.log("root администратор успешно создан.")
+        } catch (e) {
+            console.log("\nСкорее всего root администратор уже создан.")
+            console.log("postgres:",e.message);
+        }
+
         app.listen(PORT, () => console.log('Server started on port =', PORT));
     } catch (e) {
         console.log(e);
